cbda3ba1aa4bb0cbc9d2e0f028e4a572ee4e70f6
Tres Seaver <tseaver@palladion.com>
2010-04-16 21:19:22 +0000

    100% test coverage, even without the doctest.

--- src/zope/event/tests.py
+++ src/zope/event/tests.py
@@ -11,14 +11,40 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""Test the event system
-
-$Id$
+""" Test the event system
 """
 import unittest
 import doctest
 
+class Test_notify(unittest.TestCase):
+
+    def setUp(self):
+        from zope.event import subscribers
+        self._old_subscribers = subscribers[:]
+        subscribers[:] = []
+
+    def tearDown(self):
+        from zope.event import subscribers
+        subscribers[:] = self._old_subscribers
+
+    def _callFUT(self, event):
+        from zope.event import notify
+        notify(event)
+
+    def test_empty(self):
+        event = object()
+        self._callFUT(event)
+
+    def test_not_empty(self):
+        from zope.event import subscribers
+        dummy = []
+        subscribers.append(dummy.append)
+        event = object()
+        self._callFUT(event)
+        self.assertEqual(dummy, [event])
+
 def test_suite():
     return unittest.TestSuite((
+        unittest.makeSuite(Test_notify),
         doctest.DocFileSuite('README.txt'),
         ))
