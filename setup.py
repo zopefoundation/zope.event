9ff18635afe78a163964f77fd5e0806e6bf543fa
Lennart Regebro <regebro@gmail.com>
2010-05-01 14:57:17 +0000

    We need the "3" classifier to show up as a Python 3 module on PyPy, "3.0" or "3.1" isn't enough. (It actually works on 3.0 too, but we don't want to support Python 3.0).

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         "Programming Language :: Python :: 2.4",
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.1",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
