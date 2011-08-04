6ee8e92563b2460043b00b8e8e128173bd1811ed
Gediminas Paulauskas <menesis@pov.lt>
2011-08-04 20:21:48 +0000

    Back to development: 3.5.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def read(*rnames):
 
 setup(
     name='zope.event',
-    version='3.5.1',
+    version='3.5.2dev',
     url='http://pypi.python.org/pypi/zope.event',
     license='ZPL 2.1',
     description='Very basic event publishing system',
