342d9f9ea24b6170cf9ff60b0c9fc3893f7b68b9
Tres Seaver <tseaver@palladion.com>
2012-05-16 16:44:43 +0000

    Prep 4.0.0 release.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
 
 setup(
     name='zope.event',
-    version='4.0.0dev',
+    version='4.0.0',
     url='http://pypi.python.org/pypi/zope.event',
     license='ZPL 2.1',
     description='Very basic event publishing system',
