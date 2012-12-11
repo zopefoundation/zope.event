a2434a8ee127d0dee3545c5d12fc1cbdd861464f
Tres Seaver <tseaver@palladion.com>
2012-12-11 22:13:33 +0000

    Add support for jython 2.7.

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: Jython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
