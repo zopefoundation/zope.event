d06ce89a176af9dae05d9f8aba07cab570d8054c
Tres Seaver <tseaver@palladion.com>
2012-12-31 18:29:09 +0000

    svb

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
 
 setup(
     name='zope.event',
-    version='4.0.2',
+    version='4.0.3dev',
     url='http://pypi.python.org/pypi/zope.event',
     license='ZPL 2.1',
     description='Very basic event publishing system',
