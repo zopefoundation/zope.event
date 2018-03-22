c23444edde95141192d46b98276de1d0206d9a97
Jason Madden <jamadden@gmail.com>
2018-03-22 10:02:55 -0500

    The sphinx docs for z.e.classhandler use `.. automodule`.
    For two reasons:
    
    1) DRY
    2) Ability to cross-reference :mod:`zope.event.classhandler`.

--- src/zope/event/classhandler.py
+++ src/zope/event/classhandler.py
@@ -45,7 +45,10 @@ import zope.event
 registry = {}
 
 def handler(event_class, handler_=None, decorator=False):
-    """Define an event handler for a (new-style) class.
+    """
+    handler(event_class, [handler]) -> None
+
+    Define an event handler for a (new-style) class.
 
     This can be called with a class and a handler, or with just a
     class and the result used as a handler decorator.
