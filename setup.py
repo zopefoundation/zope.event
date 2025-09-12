780e8cfd956e1758e4b7b3beaa11ce81c1715c48
Michael Howitz <icemac@gmx.net>
2025-09-12 09:10:17 +0200

    Back to development: 6.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def read(*rnames):
 
 setup(
     name='zope.event',
-    version='6.0',
+    version='6.1.dev0',
     url='https://github.com/zopefoundation/zope.event',
     license='ZPL-2.1',
     description='Very basic event publishing system',
